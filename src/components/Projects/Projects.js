f71f12b207524230a9b33af69407b94ac7cff527
Anand4299 <anandbbhatt11498@gmail.com>
2024-10-03 23:49:55 +0530

    first commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import todolist from "../../Assets/Projects/preview-page0.jpg";
-<<<<<<< HEAD
-import TRAVEL from "../../Assets/Projects/travel.png"
-=======
-import FlightBooking from "../../Assets/Project/best-flight-booking-app(1).png
->>>>>>> origin/main
-
+import Travel from "../../Assets/Projects/travel.png"
 
 function Projects() {
   return (
@@ -24,8 +19,8 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
          <Col md={4} className="project-card">
             <ProjectCard
-<<<<<<< HEAD
-              imgPath={TRAVEL}
+
+              imgPath={Travel}
               isBlog={false}
               title="help to booking fligh and Booking trip"
               description="AnandTravel web is provide best trip plan provide  "
@@ -33,14 +28,7 @@ function Projects() {
               demoLink="https://anandtravel.netlify.app"
             />
           </Col>
-=======
-              imgPath={Flightbooking}
-              isBlog={false}
-              title="Flight_Booking"
-              description="todo list use to tasks as completed to stay motivated work"
-              ghLink="https://github.com/Anand4299/Travel"
-              demoLink="https://anandtravel.netlify.app"
->>>>>>> origin/main
+            
 
             <Col md={4} className="project-card">
             <ProjectCard
@@ -48,11 +36,8 @@ function Projects() {
               isBlog={false}
               title="to_do_list"
               description="todo list use to tasks as completed to stay motivated work"
-<<<<<<< HEAD
-              ghLink="https://github.com/Anand4299/TO-DO-list"
-=======
               ghLink="https://https://github.com/Anand4299/TO-DO-list"
->>>>>>> origin/main
+
               demoLink="https://to-do-list-by-anand.netlify.app/"
             />
           </Col>
